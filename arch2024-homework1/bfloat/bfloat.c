fdd8b25aac7f299059174c49a03041b1cee0b799
imNCNUwilliam <william.su.2022@gamil.com>
2024-10-12 19:05:09 +0800

    Complete the bfloat16 to float conversion as well as the automated test

--- arch2024-homework1/bfloat/bfloat.c
+++ arch2024-homework1/bfloat/bfloat.c
@@ -30,15 +30,27 @@ static inline bf16_t fp32_to_bf16(float s)
 }
 
 int main(void) {
-    float f[3] = {1, 1.25, 4};
-    int i;
+    float f[3] = {-0.5, -0.25, -0.125};
+    bf16_t bf[3];
+    int j;
 
-    for (i = 0; i < 3; i++) {
+    printf("Conversion Test ... \n");
+    printf("float to bfloat16 ... \n");
+    for (j = 0; j < 3; j++) {
         union {
             float f;
             uint32_t i;
-        } u = {.f = f[i]};
-        printf("%x %x\n", u.i, fp32_to_bf16(f[i]).bits);
+        } u = {.f = f[j]};
+        bf[j] = fp32_to_bf16(f[j]);
+        printf("%x %x\n", u.i, bf[j].bits);
+    }
+    printf("bfloat16 to float ... \n");
+    for (j = 0; j < 3; j++) {
+        union {
+            float f;
+            uint32_t i;
+        } u = {.f = bf16_to_fp32(bf[j])};
+        printf("%x %x\n", bf[j].bits, u.i);
     }
 
     return 0;
--- arch2024-homework1/bfloat/bfloat.s
+++ arch2024-homework1/bfloat/bfloat.s
@@ -2,33 +2,36 @@
     # initialize the data section with the number to be converted
     # default convertion from float to bfloat16 (direction == 0)
     nums:	.word 0x3f800000, 0x3fa00000, 0x40800000
+    nums_bf:	.word 0x3f80, 0x3fa0, 0x4080
     numsSize:	.word 3
-    direction:	.word 0
+    direction:	.word 1
     mask1:	.word 0x7fffffff
     mask2:	.word 0x7f800001
     large_num:	.word 0x7fff
 
 .text
     # Begin the main code in the text section
-    la a0, nums		# load the nums array address to register a0
-    lw a1, numsSize	# load the nums array size to register a1
-    addi a2, x0, 0	# keep the element index in register a2
-    lw s0, direction	# load the given direction into register s0
+    lw s0, direction		# load the given direction into register s0
+    la a0, nums			# load the nums array address to register a0
+    lw a1, numsSize		# load the array size to register a1
+    addi a2, x0, 0		# keep the element index in register a2
+    beq s0, x0, loop		# determine whether to switch the conversion direction
+    la a0, nums_bf		# load the nums_bf array address to register a0
 
 loop:
-    lw t1, 0(a0)	# load the nums[i] into register t1
+    lw t1, 0(a0)		# load the i-th array element into register t1
     # determine the conversion type
     bne s0, x0, bf16_to_fp32
 
 fp32_to_bf16:
-    lw t2, mask1	# load mask1 into register t2
-    lw t3, mask2	# load mask2 into register t3
-    and t4, t1, t2	# t4 = t1 & t2
-    srli t5, t1, 0x10	# extract the first 16 bits
-    bge t4, t3, quiet	# jump to quiet for special case NaN
+    lw t2, mask1		# load mask1 into register t2
+    lw t3, mask2		# load mask2 into register t3
+    and t4, t1, t2		# t4 = t1 & t2
+    srli t5, t1, 0x10		# extract the first 16 bits
+    bge t4, t3, quiet		# jump to quiet for special case NaN
 
     # Rounding related
-    andi t5, t5, 1	# examine whether the 16th bit is odd
+    andi t5, t5, 1		# examine whether the 16th bit is odd
     lw t4 large_num
     add t5, t5, t4
     add t5, t5, t1
@@ -42,11 +45,13 @@ quiet:
     j out
 
 bf16_to_fp32:
+    # convert to 4-byte
+    slli t1 t1 0x10
 
 out:
-    # result hold in register t6
-    addi a2, a2, 1	# increase the loop index
-    addi a0, a0, 4	# get the next nums[i] address
-    blt a2, a1, loop	# continue the next conversion
-    nop			# No operation, acts as a placeholder
+    # result hold in register t5 (fp32_to_bf16) or t1 (bf16_to_fp32)
+    addi a2, a2, 1		# increase the loop index
+    addi a0, a0, 4		# get the address of next array element
+    blt a2, a1, loop		# determine whether to continue the next conversion
+    nop				# No operation, acts as a placeholder
 
